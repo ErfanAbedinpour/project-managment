d950677c238e152bf24955580716cb634694d111
erfanAbedinpour <erfan.wtf44@gmail.com>
2024-10-25 14:19:43 -0400

    imported prisma and auth Module into main Module

--- src/app.module.ts
+++ src/app.module.ts
@@ -1,9 +1,11 @@
 import { Module } from '@nestjs/common';
 import {ConfigModule} from '@nestjs/config';
+import { AuthModule } from './auth/auth.module';
+import { PrismaModule } from './prisma/prisma.module';
 
 
 @Module({
-  imports: [ConfigModule.forRoot()],
+  imports: [ConfigModule.forRoot(),AuthModule,PrismaModule],
   controllers: [],
   providers: [],
 })
