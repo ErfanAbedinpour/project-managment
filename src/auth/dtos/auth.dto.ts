e9120fa9542db9ef86a702a84009f16e3fb9cb8c
erfanAbedinpour <erfan.wtf44@gmail.com>
2024-10-26 09:55:50 -0400

    added couple of dtos

--- src/auth/dtos/auth.dto.ts
+++ src/auth/dtos/auth.dto.ts
@@ -1,22 +1,56 @@
-import { IsEmail,  IsString, MaxLength, MinLength} from "class-validator";
+import {
+  IsEmail,
+  IsNotEmpty,
+  isString,
+  IsString,
+  MaxLength,
+  MinLength,
+} from 'class-validator';
+
+export class UserDTO{
+    //display name
+    @IsNotEmpty()
+    display_name:string
+    //email
+    @IsNotEmpty()
+    @IsEmail()
+    email:string;
+    //username
+    @IsNotEmpty()
+    @IsString()
+    username:string;
+    @IsNotEmpty()
+    @IsString()
+    profile:string
+}
 
 export class CreateUserDTO{
-    //email 
-    @IsEmail()
+  //email
+  @IsEmail()
+  @IsNotEmpty()
+  email: string;
+  //password
+  @MinLength(8)
+  @MaxLength(80)
+  @IsNotEmpty()
+  password: string;
+  //username
+  @MaxLength(50)
+  @MinLength(3)
+  @IsNotEmpty()
+  username: string;
+  @MaxLength(80)
+  @MinLength(3)
+  @IsNotEmpty()
+  display_name: string;
+}
+
+export class LoginUserDTO{
     @IsString()
-    email:string;
-    //password
+    @IsNotEmpty()
+    identify:string; // email or username
     @IsString()
     @MinLength(8)
     @MaxLength(80)
     password:string;
-    //username
-    @IsString()
-    @MaxLength(50)
-    @MinLength(3)
-    username:string;
-    @MaxLength(80)
-    @MinLength(3)
-    @IsString()
-    display_name:string
 }
