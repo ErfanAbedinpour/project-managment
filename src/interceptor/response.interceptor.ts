926c72b0dbaace2452242420cd972f450bf33ebb
erfanAbedinpour <erfan.wtf44@gmail.com>
2024-10-26 12:11:36 -0400

    added serialize Interceptor

---
+++ src/interceptor/response.interceptor.ts
@@ -0,0 +1,22 @@
+import { CallHandler, ExecutionContext, NestInterceptor, UseInterceptors } from "@nestjs/common";
+import { plainToClass } from "class-transformer";
+import { map, Observable} from "rxjs";
+
+interface IClass{
+    new (...args:any[]):{}
+}
+export function ResponseSerializer(dto:IClass){
+   return UseInterceptors(new SerializeResponse(dto)) 
+}
+class SerializeResponse implements NestInterceptor{
+    constructor(private dto:IClass){}
+    intercept(context: ExecutionContext, next: CallHandler<any>): Observable<any> | Promise<Observable<any>> {
+        return next.handle().pipe(
+            map((data:unknown)=>{
+                const serializeResp = plainToClass(this.dto,data,{excludeExtraneousValues:true,enableImplicitConversion:true})
+                console.log(serializeResp)
+                return serializeResp
+            })
+        )    
+    }
+}
