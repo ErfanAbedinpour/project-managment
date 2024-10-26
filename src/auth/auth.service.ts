0fe492ec960bf51b1e1609660d3afbcb707df862
erfanAbedinpour <erfan.wtf44@gmail.com>
2024-10-26 09:14:03 -0400

    added displayName to dto and check unqiue username

--- src/auth/auth.service.ts
+++ src/auth/auth.service.ts
@@ -14,13 +14,18 @@ export class AuthService{
             const isEmailTaken = await this.userService.user({email:user.email});
             if(isEmailTaken) 
                 throw new BadRequestException('email already taken.');
+
+            const isValidUserNmae = await this.userService.user({username:user.username});
+            if(isValidUserNmae) 
+                throw new BadRequestException("username already taken.");
             
             const hashPassword = await this.util.hash(user.password);
 
             const newUser= await this.userService.createUser({
                 username:user.username,
                 email:user.email,
-                password:hashPassword
+                password:hashPassword,
+                display_name:user.display_name,
             })
             return newUser;
         }catch(err){
--- src/auth/dtos/auth.dto.ts
+++ src/auth/dtos/auth.dto.ts
@@ -1,4 +1,4 @@
-import { IsEmail,  IsString, Max, Min } from "class-validator";
+import { IsEmail,  IsString, MaxLength, MinLength} from "class-validator";
 
 export class CreateUserDTO{
     //email 
@@ -7,8 +7,16 @@ export class CreateUserDTO{
     email:string;
     //password
     @IsString()
+    @MinLength(8)
+    @MaxLength(80)
     password:string;
     //username
     @IsString()
+    @MaxLength(50)
+    @MinLength(3)
     username:string;
+    @MaxLength(80)
+    @MinLength(3)
+    @IsString()
+    display_name:string
 }
