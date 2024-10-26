85dc9fd9400955607e87b0b215ad2434f01135cb
erfanAbedinpour <erfan.wtf44@gmail.com>
2024-10-26 10:12:57 -0400

    config envoriments

--- src/app.module.ts
+++ src/app.module.ts
@@ -3,10 +3,11 @@ import {ConfigModule} from '@nestjs/config';
 import { AuthModule } from './auth/auth.module';
 import { PrismaModule } from './prisma/prisma.module';
 import { UserModule } from './user/user.module';
+import { join } from 'path';
 
 
 @Module({
-  imports: [ConfigModule.forRoot(),AuthModule,PrismaModule,UserModule],
+  imports: [ConfigModule.forRoot({cache:true,isGlobal:true,envFilePath:join(__dirname,'..','.env')}),AuthModule,PrismaModule,UserModule],
   controllers: [],
   providers: [],
 })
