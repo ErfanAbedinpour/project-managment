9bbf124c7a575585507a38b586d6f550e4fab890
erfanAbedinpour <erfan.wtf44@gmail.com>
2024-11-02 09:17:37 -0400

    fix configModule

--- src/app.module.ts
+++ src/app.module.ts
@@ -1,14 +1,20 @@
 import { Module } from '@nestjs/common';
-import {ConfigModule} from '@nestjs/config';
+import { ConfigModule } from '@nestjs/config';
 import { AuthModule } from './auth/auth.module';
 import { PrismaModule } from './prisma/prisma.module';
 import { UserModule } from './user/user.module';
 import { join } from 'path';
 
-
 @Module({
   imports: [
-    ConfigModule.forRoot({cache:true, envFilePath:join(process.cwd(),`.env.test`)}),
-    AuthModule,PrismaModule,UserModule],
+    ConfigModule.forRoot({
+      cache: true,
+      isGlobal: true,
+      envFilePath: join(process.cwd(), `.env.${process.env.NODE_ENV}`),
+    }),
+    AuthModule,
+    PrismaModule,
+    UserModule,
+  ],
 })
 export class AppModule {}
