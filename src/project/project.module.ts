f28101ae379843dff6b20d51db8dbe8486155de0
erfanAbedinpour <erfan.wtf44@gmail.com>
2024-11-02 09:23:13 -0400

    exportes Project Serice

--- src/project/project.module.ts
+++ src/project/project.module.ts
@@ -1,9 +1,11 @@
 import { Module } from '@nestjs/common';
 import { ProjectController } from './project.controller';
 import { UserModule } from 'src/user/user.module';
+import { ProjectService } from './project.service';
 
 @Module({
   controllers: [ProjectController],
   imports: [UserModule],
+  exports: [ProjectService],
 })
 export class ProjectModule {}
