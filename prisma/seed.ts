afb3afc82d7c032b6c050538a74fa4bff67cea51
erfanAbedinpour <erfan.wtf44@gmail.com>
2024-11-02 11:16:45 -0400

    added seed

---
+++ prisma/seed.ts
@@ -0,0 +1,40 @@
+import { Prisma, PrismaClient } from '@prisma/client';
+import { config } from 'dotenv';
+import { join } from 'path';
+import { IEnvironmentVariables } from '../src/type';
+import { UtilService } from '../src/util/util.service';
+
+config({ path: join(process.cwd(), '.env.dev') });
+const prisma = new PrismaClient();
+declare global{
+  namespace Nodejs{
+      interface ProcessEnv extends IEnvironmentVariables {}
+  }
+}
+
+
+
+async function seed() {
+  const util = new UtilService();
+
+  const hashPass = await util.hash(process.env.ADMIN_PASSWORD);
+
+  const user: Prisma.UserCreateInput = {
+    username: process.env.ADMIN_USERNAME,
+    email: process.env.ADMIN_EMAIL,
+    display_name: 'admin',
+    password: hashPass,
+  };
+
+  await prisma.user.create({ data: user });
+}
+
+seed()
+  .then(async () => {
+    await prisma.$disconnect();
+  })
+  .catch(async (e) => {
+    console.error(e);
+    await prisma.$disconnect();
+    process.exit(1);
+  });
