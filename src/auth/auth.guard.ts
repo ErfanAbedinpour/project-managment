411e2830ae156000b25864ba7b798d3ad8523455
erfanAbedinpour <erfan.wtf44@gmail.com>
2024-11-02 09:18:22 -0400

    changed guard message

--- src/auth/auth.guard.ts
+++ src/auth/auth.guard.ts
@@ -1,31 +1,40 @@
-import { CanActivate, ExecutionContext, ForbiddenException, Injectable, UnauthorizedException } from "@nestjs/common";
-import { ConfigService } from "@nestjs/config";
-import { JsonWebTokenError, JwtService } from "@nestjs/jwt";
-import { Request } from "express";
-import { TokenPaylaod } from "./dtos/auth.dto";
+import {
+  CanActivate,
+  ExecutionContext,
+  ForbiddenException,
+  Injectable,
+  UnauthorizedException,
+} from '@nestjs/common';
+import { ConfigService } from '@nestjs/config';
+import { JsonWebTokenError, JwtService } from '@nestjs/jwt';
+import { Request } from 'express';
+import { TokenPaylaod } from './dtos/auth.dto';
 
 @Injectable()
 export class IsAuth implements CanActivate {
-    constructor(private readonly jwt: JwtService, private readonly env: ConfigService) { }
-    async canActivate(context: ExecutionContext): Promise<boolean> {
-        const request = context.switchToHttp().getRequest<Request>();
-        
-        const [type, token] = request.headers.authorization?.split(' ') ?? [];
-        const jwtToken = type === 'Bearer' ? token : null;
+  constructor(
+    private readonly jwt: JwtService,
+    private readonly env: ConfigService,
+  ) {}
+  async canActivate(context: ExecutionContext): Promise<boolean> {
+    const request = context.switchToHttp().getRequest<Request>();
 
-        if (!jwtToken) 
-            throw new UnauthorizedException("header is empty. or is not Bearer");
+    const [type, token] = request.headers.authorization?.split(' ') ?? [];
+    const jwtToken = type === 'Bearer' ? token : null;
 
-        try {
-            const payload:TokenPaylaod = await this.jwt.verifyAsync(jwtToken, {
-                secret: this.env.getOrThrow('SECRET')
-            });
-            request.user = payload;
-            return true;
-        } catch (err) {
-            throw new ForbiddenException('token is expired. ');
-        }
+    if (!jwtToken)
+      throw new UnauthorizedException(
+        'header is empty. or is not valid please etner Bearrer ',
+      );
+
+    try {
+      const payload: TokenPaylaod = await this.jwt.verifyAsync(jwtToken, {
+        secret: this.env.getOrThrow('SECRET'),
+      });
+      request.user = payload;
+      return true;
+    } catch (err) {
+      throw new ForbiddenException('token is expired. ');
     }
+  }
 }
-
-
