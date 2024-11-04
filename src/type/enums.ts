8b6434ab5ea7232a10867e8d53c628d1dbf9b532
erfanAbedinpour <erfan.wtf44@gmail.com>
2024-11-04 15:14:12 -0500

    added enums Nedded

---
+++ src/type/enums.ts
@@ -0,0 +1,25 @@
+export enum USER_ROLE {
+  ADMIN="ADMIN" ,
+  USER="USER",
+  PROJECT_MANGER="PROJECT_MANGER"
+}
+
+
+export enum PRIORITY {
+  HIGH="HIGH",
+  MIN="MID",
+  LOW="LOW"
+}
+
+export enum TASK_STATUS {
+  PENDINDG="PENDING",
+  DONE="DONE",
+  INQUEUE="INQUEUE"
+}
+
+export enum PROJECT_STATUS  {
+  ACTIVE="ACTIVE",
+   DIACTIVE="DIACTIVE",
+   COMPLETE="COMPLETE"
+}
+
