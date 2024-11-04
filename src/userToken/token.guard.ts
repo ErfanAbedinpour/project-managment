d3ebec889e9fd3f201380e9ee604fc7181be7a7a
erfanAbedinpour <erfan.wtf44@gmail.com>
2024-11-04 08:20:48 -0500

    updated token.guard

--- src/userToken/token.guard.ts
+++ src/userToken/token.guard.ts
@@ -1,17 +1,21 @@
 import { CanActivate, ExecutionContext, Injectable, UnauthorizedException } from "@nestjs/common";
 import { JwtCustomeService } from "./jwt.service";
 import { JsonWebTokenError } from "@nestjs/jwt";
+import { UserTokenService } from "./userToken.service";
 
 
 @Injectable()
 export class TokenGuard implements CanActivate{
-    constructor(private readonly jwt:JwtCustomeService){}
+    constructor(private readonly jwt:JwtCustomeService, private readonly userTokenService:UserTokenService){}
     async canActivate(context: ExecutionContext): Promise<boolean>  {
         const req = context.switchToHttp().getRequest();
         const {refreshToken}= req.body;
         try{
-            const payload = await this.jwt.verifyRefreshToken(refreshToken);
-            return !!payload
+            //verify token 
+            await this.jwt.verifyRefreshToken(refreshToken);
+            //is refreshToken store in DB
+            const isTokenValid = await this.userTokenService.getUserByToken(refreshToken);
+            return !!isTokenValid
         }catch(err){
             if(err instanceof JsonWebTokenError)
                 throw new UnauthorizedException("token is invalid. please login again.")
