0a834b64e3afd1f0bc1afa423314809a3b06c6d5
erfanAbedinpour <erfan.wtf44@gmail.com>
2024-11-04 07:45:48 -0500

    move user.decoator into UserModuel

---
+++ src/user/user.decorator.ts
@@ -0,0 +1,11 @@
+import { createParamDecorator, ExecutionContext, ForbiddenException } from "@nestjs/common";
+import { Request } from "express";
+
+export const CurentUser = createParamDecorator(
+    (_:unknown,ctx:ExecutionContext)=>{
+        const req= ctx.switchToHttp().getRequest<Request>();
+        if(!req.user) throw new ForbiddenException();
+
+        return req.user; 
+    }
+)
