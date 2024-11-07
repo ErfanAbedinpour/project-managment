b7a6d1b680ec332a040535c966a7c64838388d22
erfanAbedinpour <erfan.wtf44@gmail.com>
2024-11-07 12:52:38 -0500

    added userUpdate Route?

--- src/user/user.controller.ts
+++ src/user/user.controller.ts
@@ -1,14 +1,66 @@
-import { Controller, Get, UseGuards } from "@nestjs/common";
-import { IsAuth } from "../auth/auth.guard";
+import { BadRequestException, Body,  Controller,  Get, Inject,  Patch, Res, UnauthorizedException, UseGuards } from "@nestjs/common";
+import {Response} from 'express';
 import { CurentUser } from "./user.decorator";
-import { AccessTokenPyload } from "../userToken/dtos/token.dto";
+import { AccessTokenPyload, UserTokenParam } from "../userToken/dtos/token.dto";
+import { UserServices } from "./user.service";
+import { UserResponseDTO, UserUpdatedBodyDTO } from "./dtos/user.dto";
+import { PrismaClientKnownRequestError } from "@prisma/client/runtime/library";
+import { IsAuth } from "../auth/auth.guard";
+import { ResponseSerializer } from "../interceptor/response.interceptor";
+import { UserTokenService } from "../userToken/userToken.service";
+
+import { Cache } from "cache-manager";
+import { CACHE_MANAGER } from "@nestjs/cache-manager";
+import { UserTokens } from "../userToken/userTokens.decorator";
 
 
 @Controller('/user')
 @UseGuards(IsAuth)
 export class UserController{
+    constructor( 
+      private readonly userService:UserServices,
+      private readonly userToken:UserTokenService,
+      @Inject(CACHE_MANAGER) private readonly cache:Cache
+    ){
+    }
+
     @Get("me")
     me(@CurentUser() me:AccessTokenPyload){
         return me;
+    }
+
+    // update user
+    @Patch()
+    @ResponseSerializer(UserResponseDTO)
+    async updateUser(
+      @CurentUser() user:AccessTokenPyload,
+      @Body() body:UserUpdatedBodyDTO,
+      @Res({passthrough:true}) res:Response,
+      @UserTokens() userToken:UserTokenParam){
+      try{
+        const newUser = await this.userService.updateUser({id:user.id,data:body});
+
+        await this.userToken.deleteToken({token:userToken.refreshToken});
+        // set accessToken into blackList
+        await this.cache.set(btoa(userToken.accessToken),userToken.accessToken);
+
+        res.clearCookie("accessToken");
+        res.clearCookie("refreshToken");
+        return newUser;
+      }catch(err){
+        if(err instanceof PrismaClientKnownRequestError){
+          const {code,meta}= err;
+          switch(code){
+            case 'P2002':
+              throw new BadRequestException(`${meta.target} is already taken.`);
+            case 'P2025':
+              throw new UnauthorizedException("please login again");
+            default:
+              console.error(err)
+              throw new BadRequestException(`${err.code} is`);
+          }
+        }
+        throw err;
+      }
     }
 }
