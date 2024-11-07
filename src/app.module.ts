d6de01679f8700477ba5eb6a927ac694a52ce24d
erfanAbedinpour <erfan.wtf44@gmail.com>
2024-11-07 15:01:20 -0500

    import nodeMailer Module

--- src/app.module.ts
+++ src/app.module.ts
@@ -1,11 +1,13 @@
 import { Module } from '@nestjs/common';
-import { ConfigModule } from '@nestjs/config';
+import { ConfigModule, ConfigService } from '@nestjs/config';
 import { AuthModule } from './auth/auth.module';
 import { PrismaModule } from './prisma/prisma.module';
 import { UserModule } from './user/user.module';
 import { join } from 'path';
 import { UserTokenModule} from './userToken/userToken.module';
 import { CacheModule } from '@nestjs/cache-manager';
+import { MailerModule } from '@nestjs-modules/mailer';
+import { IEnvironmentVariables } from './type';
 
 @Module({
   imports: [
@@ -21,7 +23,21 @@ import { CacheModule } from '@nestjs/cache-manager';
     CacheModule.register({
       isGlobal:true,
       ttl:30*60*1000
+    }),
+    MailerModule.forRootAsync({
+      useFactory:(env:ConfigService<IEnvironmentVariables>)=>{
+        return {
+          transport:{
+            host:env.getOrThrow<string>("EMAIL_HOST"),
+            auth:{
+              user:env.getOrThrow<string>("EMAIL_USERNAME"),
+              pass:env.getOrThrow<string>("EMAIL_PASSWORD") 
+            }
+          }
+        }
+      },
+      inject:[ConfigService]
     })
-  ],
+   ],
 })
 export class AppModule {}
