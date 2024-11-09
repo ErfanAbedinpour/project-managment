a00a06994016a02bf57875f731fce4f70fde099a
erfanAbedinpour <erfan.wtf44@gmail.com>
2024-11-09 12:52:27 -0500

    added update user into service and use them form controller

--- src/user/user.service.ts
+++ src/user/user.service.ts
@@ -1,4 +1,4 @@
-import { BadRequestException, Inject, Injectable } from '@nestjs/common';
+import { BadRequestException, HttpException, Inject, Injectable } from '@nestjs/common';
 import { Prisma, User } from '@prisma/client';
 import { PrismaService } from '../prisma/prisma.service';
 import { MailerService } from '@nestjs-modules/mailer';
@@ -6,14 +6,16 @@ import { UtilService } from '../util/util.service';
 import { SentMessageInfo } from 'nodemailer';
 import { CACHE_MANAGER } from '@nestjs/cache-manager';
 import { Cache } from 'cache-manager';
+import { UserTokenService } from '../userToken/userToken.service';
 
 @Injectable()
 export class UserServices {
   constructor(
-      private readonly prisma: PrismaService,
-      private readonly mailer:MailerService,
-      private readonly util:UtilService,
-      @Inject(CACHE_MANAGER) private readonly cache:Cache,
+    private readonly prisma: PrismaService,
+    private readonly mailer: MailerService,
+    private readonly util: UtilService,
+    @Inject(CACHE_MANAGER) private readonly cache: Cache,
+    private readonly userToken: UserTokenService
   ) { }
 
   async user(
@@ -45,12 +47,31 @@ export class UserServices {
     });
   }
 
-  async updateUser(params:{id:number, data: Prisma.UserUpdateInput}): Promise<Omit<User,'password'>> {
-    const {id,data} = params;
-    return this.prisma.user.update({
-      where:{id:id},
-      data:data,
-    })
+  async updateUser(params: { id: number, data: Prisma.UserUpdateInput, refreshToken: string, accessToken: string }): Promise<{ id: number, username: string, display_name: string; profile: string }> {
+    const { id, data, refreshToken, accessToken } = params;
+    try {
+      const newUser = await this.prisma.user.update({
+        where: {
+          id: id
+        },
+        data: data,
+        select: {
+          id: true,
+          email: true,
+          username: true,
+          display_name: true,
+          profile: true
+        }
+      })
+      //remove user Token
+      await this.userToken.deleteToken({ token: refreshToken });
+      // add userAccessToken to blackList
+      await this.cache.set(btoa(accessToken), accessToken);
+
+      return newUser;
+    } catch (err) {
+      throw err;
+    }
   }
 
   async deleteUser(where: Prisma.UserWhereUniqueInput): Promise<User> {
@@ -59,30 +80,30 @@ export class UserServices {
     });
   }
 
-  async deleteAccount(userId:number):Promise<{success:boolean,mailInfo:SentMessageInfo}>{
-    try{
-      const user =await this.user({id:userId});
-      if(!user)
+  async deleteAccount(userId: number): Promise<{ success: boolean, mailInfo: SentMessageInfo }> {
+    try {
+      const user = await this.user({ id: userId });
+      if (!user)
         throw new BadRequestException("user does not found");
 
-      let code =this.util.generateUniqueCode(10000,99999);
-      await this.cache.set(user.id.toString(),code);
+      let code = this.util.generateUniqueCode(10000, 99999);
+      await this.cache.set(user.id.toString(), code);
       // TODO: check if key is duplicate value is replace with that or not?
 
       const info = await this.mailer.sendMail({
-        to:user.email,
-        from:"Mini Github",
-        subject:"Delete account Verification Code",
-        text:`Your verification code is ${code}`,
+        to: user.email,
+        from: "Mini Github",
+        subject: "Delete account Verification Code",
+        text: `Your verification code is ${code}`,
       })
 
-      return {success:true,mailInfo:info}
+      return { success: true, mailInfo: info }
 
-    }catch(err){
+    } catch (err) {
       throw err;
     }
   }
- 
-  
-  async verifyCode(code:number){}
+
+
+  async verifyCode(code: number) { }
 }
