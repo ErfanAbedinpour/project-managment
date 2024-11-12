e2e812f933222f02f9a4caeb6a8837112643a4ab
erfanAbedinpour <erfan.wtf44@gmail.com>
2024-11-12 13:33:28 -0500

    remove uneccesery line

--- src/auth/auth.guard.ts
+++ src/auth/auth.guard.ts
@@ -2,15 +2,13 @@ import {
   CanActivate,
   ExecutionContext,
   HttpException,
-  Inject,
   Injectable,
   UnauthorizedException,
 } from '@nestjs/common';
 import { Request } from 'express';
 import { JwtCustomeService } from '../userToken/jwt.service';
 import { AccessTokenPyload } from '../userToken/dtos/token.dto';
-import { plainToInstance } from 'class-transformer';
-import { validateOrReject } from 'class-validator';
+
 
 @Injectable()
 export class IsAuth implements CanActivate {
@@ -27,11 +25,9 @@ export class IsAuth implements CanActivate {
       throw new UnauthorizedException('you cannot access to this router please login first. ');
 
     try {
-      const payload: unknown = await this.jwtService.verifyAccessToken(accessToken);
-      const user = plainToInstance(AccessTokenPyload, payload)
-      await validateOrReject(user)
 
-      request.user = user;
+      const payload: AccessTokenPyload = await this.jwtService.verifyAccessToken(accessToken);
+      request.user = payload;
       return true;
     } catch (err) {
       if (err instanceof HttpException)
