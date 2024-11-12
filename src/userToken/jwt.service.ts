521390d8f34948de31d4a410260ccab1fbc2fb3b
erfanAbedinpour <erfan.wtf44@gmail.com>
2024-11-12 13:12:12 -0500

    before validation check token is in BlackList

--- src/userToken/jwt.service.ts
+++ src/userToken/jwt.service.ts
@@ -1,42 +1,46 @@
-import { Injectable } from "@nestjs/common";
+import { ForbiddenException, Inject, Injectable } from "@nestjs/common";
 import { ConfigService } from "@nestjs/config";
 import { JwtService } from "@nestjs/jwt";
 import { IEnvironmentVariables } from "../type";
 import { AccessTokenPyload, RefreshTokenPayload } from "./dtos/token.dto";
+import { Cache } from "cache-manager";
+import { CACHE_MANAGER } from "@nestjs/cache-manager";
 
 
 
 @Injectable()
-export class JwtCustomeService{
+export class JwtCustomeService {
     private readonly accessTokenJwt: JwtService;
     private readonly refreshTokenJwt: JwtService;
 
-    constructor(private  readonly config:ConfigService<IEnvironmentVariables>) {
+    constructor(private readonly config: ConfigService<IEnvironmentVariables>, @Inject(CACHE_MANAGER) private readonly cache: Cache) {
         this.accessTokenJwt = new JwtService({
-            secret:this.config.getOrThrow<string>("ACCESS_TOKEN_SECRET"),
-            signOptions:{expiresIn:this.config.getOrThrow<string>("ACCESS_TOKEN_EXPIRE")+'m'}
+            secret: this.config.getOrThrow<string>("ACCESS_TOKEN_SECRET"),
+            signOptions: { expiresIn: this.config.getOrThrow<string>("ACCESS_TOKEN_EXPIRE") + 'm' }
         })
 
 
-        this.refreshTokenJwt= new JwtService({
-            secret:this.config.getOrThrow<string>("REFRESH_TOKEN_SECRET"),
-            signOptions:{expiresIn:this.config.getOrThrow<string>("REFRESH_TOKEN_EXPIRE")+'d'}
+        this.refreshTokenJwt = new JwtService({
+            secret: this.config.getOrThrow<string>("REFRESH_TOKEN_SECRET"),
+            signOptions: { expiresIn: this.config.getOrThrow<string>("REFRESH_TOKEN_EXPIRE") + 'd' }
         })
     }
 
-    async signAccessToken(payload: AccessTokenPyload):Promise<string> {
+    async signAccessToken(payload: AccessTokenPyload): Promise<string> {
         return this.accessTokenJwt.signAsync(payload);
     }
 
-    async signRefreshToken(payload: RefreshTokenPayload):Promise<string> {
+    async signRefreshToken(payload: RefreshTokenPayload): Promise<string> {
         return this.refreshTokenJwt.signAsync(payload);
     }
 
-    async verifyRefreshToken(token: string):Promise<RefreshTokenPayload> {
+    async verifyRefreshToken(token: string): Promise<RefreshTokenPayload> {
         return this.refreshTokenJwt.verifyAsync(token);
     }
 
-    async verifyAccessToken(token: string):Promise<AccessTokenPyload> {
+    async verifyAccessToken(token: string): Promise<AccessTokenPyload> {
+        if (!(await this.cache.get<string>(btoa(token))))
+            throw new Error("token is in blackList");
         return this.accessTokenJwt.verifyAsync(token);
     }
 }
--- src/userToken/userToken.module.ts
+++ src/userToken/userToken.module.ts
@@ -1,27 +1,28 @@
-import {  Module } from "@nestjs/common";
-import { UserTokenService} from "./userToken.service";
+import { Module } from "@nestjs/common";
+import { UserTokenService } from "./userToken.service";
 import { UtilModule } from "../util/util.module";
 import { PrismaModule } from "../prisma/prisma.module";
 import { JwtCustomeService } from "./jwt.service";
 import { JwtModule } from "@nestjs/jwt";
+import { CacheModule } from "@nestjs/cache-manager";
 
 
 
 @Module({
-    imports:[
+    imports: [
         PrismaModule,
         UtilModule,
-        JwtModule.register({})
+        JwtModule.register({}),
     ],
 
-    exports:[
+    exports: [
         UserTokenService,
         JwtCustomeService
     ],
 
-    providers:[
+    providers: [
         UserTokenService,
         JwtCustomeService
     ],
 })
-export class UserTokenModule{}
+export class UserTokenModule { }
