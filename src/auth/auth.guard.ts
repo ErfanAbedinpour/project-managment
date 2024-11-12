608654af8064db4e6ed40ce514b51a47e4510ced
erfanAbedinpour <erfan.wtf44@gmail.com>
2024-11-12 13:15:45 -0500

    remove cache check in gurad

--- src/auth/auth.guard.ts
+++ src/auth/auth.guard.ts
@@ -1,7 +1,6 @@
 import {
   CanActivate,
   ExecutionContext,
-  ForbiddenException,
   HttpException,
   Inject,
   Injectable,
@@ -11,38 +10,31 @@ import { Request } from 'express';
 import { JwtCustomeService } from '../userToken/jwt.service';
 import { AccessTokenPyload } from '../userToken/dtos/token.dto';
 import { plainToInstance } from 'class-transformer';
-import {  validateOrReject } from 'class-validator';
-import { CACHE_MANAGER } from '@nestjs/cache-manager';
-import { Cache } from 'cache-manager';
+import { validateOrReject } from 'class-validator';
 
 @Injectable()
 export class IsAuth implements CanActivate {
   constructor(
-    private readonly jwtService:JwtCustomeService,
-    @Inject(CACHE_MANAGER) private readonly cache:Cache
-  ) {}
+    private readonly jwtService: JwtCustomeService,
+  ) { }
 
   async canActivate(context: ExecutionContext): Promise<boolean> {
     const request = context.switchToHttp().getRequest<Request>();
 
-    const {accessToken,refreshToken} = request.cookies;
+    const { accessToken, refreshToken } = request.cookies;
 
     if (!accessToken || !refreshToken)
       throw new UnauthorizedException('you cannot access to this router please login first. ');
-    
+
     try {
-      // check token is in balckList
-      const isInBlackList = await this.cache.get<string>(btoa(accessToken)) ;
-      if(isInBlackList)
-        throw new Error();
-      const payload:unknown = await this.jwtService.verifyAccessToken(accessToken);
-      const user = plainToInstance(AccessTokenPyload,payload)
+      const payload: unknown = await this.jwtService.verifyAccessToken(accessToken);
+      const user = plainToInstance(AccessTokenPyload, payload)
       await validateOrReject(user)
 
       request.user = user;
       return true;
     } catch (err) {
-      if(err instanceof HttpException)
+      if (err instanceof HttpException)
         throw err;
       throw new UnauthorizedException('token is invalid or expired. please get new. ');
     }
