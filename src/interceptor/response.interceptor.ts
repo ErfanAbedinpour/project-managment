0e45d89f9c7402e22a3647244d1ce158b45ae2da
erfanAbedinpour <erfan.wtf44@gmail.com>
2024-11-16 10:50:03 -0500

    remove bulshit interceptor

--- src/interceptor/response.interceptor.ts
+++ src/interceptor/response.interceptor.ts
@@ -1,21 +1,21 @@
 import { CallHandler, ExecutionContext, NestInterceptor, UseInterceptors } from "@nestjs/common";
 import { plainToClass } from "class-transformer";
-import { map, Observable} from "rxjs";
+import { map, Observable } from "rxjs";
 
-interface IClass{
-    new (...args:any[]):{}
+interface IClass {
+    new(...args: any[]): {}
 }
-export function ResponseSerializer(dto:IClass){
-   return UseInterceptors(new SerializeResponse(dto)) 
+export function ResponseSerializer(dto: IClass) {
+    return UseInterceptors(new SerializeResponse(dto))
 }
-class SerializeResponse implements NestInterceptor{
-    constructor(private dto:IClass){}
-    intercept(context: ExecutionContext, next: CallHandler<any>): Observable<any> | Promise<Observable<any>> {
+class SerializeResponse implements NestInterceptor {
+    constructor(private dto: IClass) { }
+    intercept(_: ExecutionContext, next: CallHandler<any>): Observable<any> | Promise<Observable<any>> {
         return next.handle().pipe(
-            map((data:unknown)=>{
-                const serializeResp = plainToClass(this.dto,data,{excludeExtraneousValues:true,enableImplicitConversion:true})
+            map((data: unknown) => {
+                const serializeResp = plainToClass(this.dto, data, { excludeExtraneousValues: true, enableImplicitConversion: true })
                 return serializeResp
             })
-        )    
+        )
     }
 }
