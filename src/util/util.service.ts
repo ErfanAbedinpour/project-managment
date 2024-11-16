f02026e6f9165be2641011359f1fcf4036b27ff8
erfanAbedinpour <erfan.wtf44@gmail.com>
2024-11-16 10:57:24 -0500

    remove hashing from util and move to another module

--- src/util/util.service.ts
+++ src/util/util.service.ts
@@ -1,27 +1,11 @@
-import {hash,genSaltSync,compare} from 'bcrypt';
+import { hash, genSaltSync, compare } from 'bcrypt';
 
-export class UtilService{
-    private readonly SALT_LEN=12;
-    hash(password:string):Promise<string>{
-        try{
-            const salt = genSaltSync(this.SALT_LEN);
-            return hash(password,salt);
-        }catch(err){
-            throw err;
-        }
+export class UtilService {
+    dayToMilisecond(day: number) {
+        return day * 24 * 60 * 60 * 1000 + Date.now();
     }
 
-    verify(password:string,hash:string):Promise<boolean>{
-        return compare(password,hash);
+    generateUniqueCode(min: number, max: number) {
+        return Math.floor(Math.random() * (max - min) + min);
     }
-
-
-    dayToMilisecond(day:number){
-        return day*24*60*60*1000 + Date.now();
-    }
-
-    generateUniqueCode(min:number,max:number){
-        return Math.floor(Math.random() * (max-min)+min);
-    }
-
 }
