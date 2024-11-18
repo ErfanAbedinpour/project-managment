48342c9c9547e4ee51f814dd63d706bff54080ca
erfanAbedinpour <erfan.wtf44@gmail.com>
2024-11-18 12:23:45 -0500

    added curentUser decorator

---
+++ src/auth/decorator/curent-user.decorator.ts
@@ -0,0 +1,11 @@
+import { createParamDecorator, ExecutionContext } from "@nestjs/common";
+import { CurentUser } from "../interface/curent-user.interface";
+import { Request } from "express";
+
+export const GetUser = createParamDecorator((
+    filed: keyof CurentUser, context: ExecutionContext
+) => {
+    const request = context.switchToHttp().getRequest<Request>();
+
+    return filed ? request?.user[filed] : request.user
+})
