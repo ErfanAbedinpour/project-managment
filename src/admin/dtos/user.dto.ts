b00345e2f1fa422d3a90e7362f7fbdfe9a69dc11
erfanAbedinpour <erfan.wtf44@gmail.com>
2024-11-21 10:38:39 -0500

    remove admin directory

--- src/admin/dtos/user.dto.ts
+++
@@ -1,9 +0,0 @@
-import { IsNotEmpty, IsString } from "class-validator";
-import { USER_ROLE } from "../../type/enums";
-import { CreateUserDTO } from "../../auth/dtos/create-user-dto";
-
-export class UserDTO extends CreateUserDTO {
-    @IsString()
-    @IsNotEmpty()
-    role: USER_ROLE
-}
