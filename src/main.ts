1c0a40e000862067a438507207a2eb9229daab3a
erfanAbedinpour <erfan.wtf44@gmail.com>
2024-11-22 12:51:52 -0500

    added swaget for api documention

--- src/main.ts
+++ src/main.ts
@@ -2,12 +2,21 @@ import { NestFactory } from '@nestjs/core';
 import { AppModule } from './app.module';
 import { ValidationPipe } from '@nestjs/common';
 import * as cookieParser from 'cookie-parser';
+import { DocumentBuilder, SwaggerModule } from '@nestjs/swagger';
 
 async function bootstrap() {
   const app = await NestFactory.create(AppModule);
   app.use(cookieParser())
   app.useGlobalPipes(new ValidationPipe({ whitelist: true }))
   app.setGlobalPrefix("api")
+  const config = new DocumentBuilder()
+    .setTitle("Mini Github")
+    .setDescription(`a mini github for manage better your project`)
+    .setVersion("1.0")
+    .addTag("project")
+    .build();
+  const docuementFactory = () => SwaggerModule.createDocument(app, config);
+  SwaggerModule.setup("api", app, docuementFactory);
   await app.listen(3000, () => console.log(`http://localhost:3000`));
 }
 bootstrap();
