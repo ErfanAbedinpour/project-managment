6dff1e5b39d657375384e0c2b668a50defb2932a
erfanAbedinpour <erfan.wtf44@gmail.com>
2024-11-24 11:29:42 -0500

    update auth.gurad

--- src/auth/__test__/auth.guard.spec.ts
+++ src/auth/__test__/auth.guard.spec.ts
@@ -4,7 +4,7 @@ import { Reflector } from "@nestjs/core";
 import { UserTokenModule } from "../../userToken/userToken.module";
 import { AccessTokenGurad } from "../gurad/accessToken.guard";
 import { DeepMockProxy, mockDeep } from "jest-mock-extended";
-import { ExecutionContext, ForbiddenException, SetMetadata } from "@nestjs/common";
+import { ExecutionContext, ForbiddenException, SetMetadata, UnauthorizedException } from "@nestjs/common";
 import { AuthStrategy } from "../decorator/auth.decorator";
 
 describe("AccessToken gurad ", () => {
@@ -15,7 +15,8 @@ describe("AccessToken gurad ", () => {
 
     const context = {
         getHandler: jest.fn(),
-        getClass: jest.fn()
+        getClass: jest.fn(),
+        switchToHttp: jest.fn()
     }
 
     let accessTokenGurad: DeepMockProxy<AccessTokenGurad>;
@@ -49,7 +50,14 @@ describe("AccessToken gurad ", () => {
         expect(accessTokenGurad).toBeDefined()
     })
 
-    it("Should be resolve true for AuthGurad is None", () => {
+    it("Should be resolve true for AuthGurad is None and header is null", () => {
+        context.switchToHttp.mockImplementationOnce(() => {
+            return {
+                getRequest: () => ({
+                    headers: { authorization: null }
+                })
+            }
+        })
         reflector.getAllAndOverride.mockImplementationOnce((token: string, handlers: Function[]) => {
             return [AuthStrategy.None]
         })
@@ -59,21 +67,39 @@ describe("AccessToken gurad ", () => {
         expect(context.getHandler).toHaveBeenCalled()
         expect(context.getClass).toHaveBeenCalledTimes(1)
         expect(context.getHandler).toHaveBeenCalledTimes(1)
+        expect(accessTokenGurad.canActivate).not.toHaveBeenCalled()
     })
 
-    it("thorw an Error if accessTOken gurad throw false", () => {
-        accessTokenGurad.canActivate.mockResolvedValue(false);
+    it("thorw an Error if accessToken gurad throw Error", () => {
+        context.switchToHttp.mockImplementationOnce(() => {
+            return {
+                getRequest: () => ({
+                    headers: { authorization: "token" }
+                })
+            }
+        })
+        accessTokenGurad.canActivate.mockRejectedValueOnce(new UnauthorizedException(""));
         reflector.getAllAndOverride.mockImplementationOnce((token: string, handlers: Function[]) => {
             return [AuthStrategy.Bearer]
         })
 
-        const resPr = gurad.canActivate(context as unknown as ExecutionContext)
-        expect(resPr).rejects.toThrow(ForbiddenException)
+        const resPr = gurad.canActivate(context as unknown as ExecutionContext);
+        expect(resPr).rejects.toThrow(UnauthorizedException)
         expect(accessTokenGurad.canActivate).toHaveBeenCalled()
         expect(accessTokenGurad.canActivate).toHaveBeenCalledTimes(1)
     })
-    it("shold be throw ok with Bearer", () => {
+
+    it("shold be pass if authGurad return true", () => {
+        context.switchToHttp.mockImplementationOnce(() => {
+            return {
+                getRequest: () => ({
+                    headers: { authorization: "token" }
+                })
+            }
+        })
+
         accessTokenGurad.canActivate.mockResolvedValue(true);
+
         reflector.getAllAndOverride.mockImplementationOnce((token: string, handlers: Function[]) => {
             return [AuthStrategy.Bearer]
         })
@@ -82,6 +108,93 @@ describe("AccessToken gurad ", () => {
         expect(resPr).resolves.toBe(true)
         expect(accessTokenGurad.canActivate).toHaveBeenCalled()
         expect(accessTokenGurad.canActivate).toHaveBeenCalledTimes(1)
+    })
+
+    it('Should be thorw Error if user set invalid header even route in open', () => {
+        context.switchToHttp.mockImplementationOnce(() => {
+            return {
+                getRequest: () => ({
+                    headers: { authorization: "token" }
+                })
+            }
+        })
+
+        accessTokenGurad.canActivate.mockResolvedValue(false);
+
+        reflector.getAllAndOverride.mockImplementationOnce((token: string, handlers: Function[]) => {
+            return [AuthStrategy.None]
+        })
+
+        const resPr = gurad.canActivate(context as unknown as ExecutionContext)
+        expect(resPr).resolves.toBe(true)
+        expect(accessTokenGurad.canActivate).toHaveBeenCalled()
+        expect(accessTokenGurad.canActivate).toHaveBeenCalledTimes(1)
+        expect(accessTokenGurad.canActivate).toHaveBeenCalled()
+    })
+
+    it('Shoule be pass from accessGurad if user set header even route is open', () => {
+        context.switchToHttp.mockImplementationOnce(() => {
+            return {
+                getRequest: () => ({
+                    headers: { authorization: "asdfasdfasdf" }
+                })
+            }
+        })
+
+        accessTokenGurad.canActivate.mockResolvedValue(true);
+
+        reflector.getAllAndOverride.mockImplementationOnce((token: string, handlers: Function[]) => {
+            return [AuthStrategy.None]
+        })
+
+        const resPr = gurad.canActivate(context as unknown as ExecutionContext)
+        expect(resPr).resolves.toBe(true)
+        expect(accessTokenGurad.canActivate).toHaveBeenCalled()
+        expect(accessTokenGurad.canActivate).toHaveBeenCalledTimes(1)
+    })
+
+    it("route is public also and private when use set header should pass form accessGurad", () => {
+        context.switchToHttp.mockImplementationOnce(() => {
+            return {
+                getRequest: () => ({
+                    headers: { authorization: "asdfasdfasdf" }
+                })
+            }
+        })
+
+        accessTokenGurad.canActivate.mockResolvedValue(true);
+
+        reflector.getAllAndOverride.mockImplementationOnce((token: string, handlers: Function[]) => {
+            return [AuthStrategy.None, AuthStrategy.Bearer]
+        })
+
+        const resPr = gurad.canActivate(context as unknown as ExecutionContext)
+        expect(resPr).resolves.toBe(true)
+
+        expect(accessTokenGurad.canActivate).toHaveBeenCalled()
+        expect(accessTokenGurad.canActivate).toHaveBeenCalledTimes(1)
+
+    })
+    it("route is public also and private when user is not set header should pass directly", () => {
+        context.switchToHttp.mockImplementationOnce(() => {
+            return {
+                getRequest: () => ({
+                    headers: { authorization: null }
+                })
+            }
+        })
+
+        accessTokenGurad.canActivate.mockResolvedValue(true);
+
+        reflector.getAllAndOverride.mockImplementationOnce((token: string, handlers: Function[]) => {
+            return [AuthStrategy.None, AuthStrategy.Bearer]
+        })
+
+        const resPr = gurad.canActivate(context as unknown as ExecutionContext)
+        expect(resPr).resolves.toBe(true)
+
+        expect(accessTokenGurad.canActivate).not.toHaveBeenCalled()
+        expect(accessTokenGurad.canActivate).toHaveBeenCalledTimes(0)
     })
 
 })
