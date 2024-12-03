7523e016b71e297ef68ce05c08b0fee686237351
erfan <erfan.wtf44@gmail.com>
2024-12-03 06:40:17 -0500

    chore: Added Kick From Project By owner

--- src/contribute/contribute.controller.ts
+++ src/contribute/contribute.controller.ts
@@ -3,6 +3,8 @@ import {
   Get,
   Param,
   Post,
+  Delete,
+  ParseIntPipe,
 } from '@nestjs/common';
 import { ContributeService } from './contribute.service';
 import { GetUser } from '../auth/decorator/curent-user.decorator';
@@ -10,18 +12,47 @@ import { Auth, AuthStrategy } from '../auth/decorator/auth.decorator';
 
 @Controller('project/:username/:projectName/contribute')
 export class ContributeController {
-  constructor(private readonly service: ContributeService) { }
+  constructor(private readonly service: ContributeService) {}
 
   @Post()
-  contributeToProject(@Param("username") username: string, @Param("projectName") name: string, @GetUser("id") userId: number) {
-    return this.service.contributeToNewProject({ username, userId, projectName: name });
+  contributeToProject(
+    @Param('username') username: string,
+    @Param('projectName') name: string,
+    @GetUser('id') userId: number,
+  ) {
+    return this.service.contributeToNewProject({
+      username,
+      userId,
+      projectName: name,
+    });
   }
 
   @Auth(AuthStrategy.None)
   @Get()
-  getProjectContributers(@Param("projectName") name: string, @Param("username") username: string, @GetUser('id') me: number) {
-    return this.service.getProjectContributers({ username, projectName: name, userId: me })
+  getProjectContributers(
+    @Param('projectName') name: string,
+    @Param('username') username: string,
+    @GetUser('id') me: number,
+  ) {
+    return this.service.getProjectContributers({
+      username,
+      projectName: name,
+      userId: me,
+    });
   }
 
-
+  @Delete(':userId')
+  kickContributer(
+    @Param('projectName') name: string,
+    @Param('username') username: string,
+    @Param('userId', ParseIntPipe) userId: number,
+    @GetUser('id') owner: number,
+  ) {
+    return this.service.kickContributer({
+      projectName: name,
+      owner: owner,
+      username: username,
+      userId,
+    });
+  }
 }
--- src/contribute/contribute.service.ts
+++ src/contribute/contribute.service.ts
@@ -3,6 +3,7 @@ import { PrismaService } from '../prisma/prisma.service';
 import {
   BadRequestException,
   ConflictException,
+  HttpException,
   Injectable,
   InternalServerErrorException,
   Logger,
@@ -12,11 +13,18 @@ import {
 import { ProjectService } from '../project/project.service';
 import { UserServices } from '../user/user.service';
 import { userInfo } from 'os';
+import { retry, throwError } from 'rxjs';
+import { Project, ProjectContributer } from '@prisma/client';
+import { ProjectController } from 'src/project/project.controller';
 
 @Injectable()
 export class ContributeService {
   private readonly projectNotFoundErr = 'project not found';
   private readonly userNotFoundErr = 'user does not found';
+  private readonly CannotAccessThisProject =
+    'you cannot access To this project';
+  private readonly ContributerDoesNotFound =
+    'this user does not contribute on this project';
   private readonly userAlreadyContributed = 'user already contributed';
   private readonly logger = new Logger(ContributeService.name);
 
@@ -24,7 +32,7 @@ export class ContributeService {
     private readonly prisma: PrismaService,
     private readonly projectService: ProjectService,
     private readonly userService: UserServices,
-  ) { }
+  ) {}
 
   private async isUserContributed(params: {
     userId: number;
@@ -36,7 +44,11 @@ export class ContributeService {
     }));
   }
 
-  async contributeToNewProject(params: { userId: number, projectName: string; username: string }): Promise<{ success: boolean }> {
+  async contributeToNewProject(params: {
+    userId: number;
+    projectName: string;
+    username: string;
+  }): Promise<{ success: boolean }> {
     const { userId, projectName, username } = params;
     // check project is exist or not
 
@@ -47,17 +59,17 @@ export class ContributeService {
     });
 
     // if project does not exist throw error
-    if (!project)
-      throw new NotFoundException(this.projectNotFoundErr);
+    if (!project) throw new NotFoundException(this.projectNotFoundErr);
 
     if (userId === project.ownerId)
-      throw new BadRequestException("You cannot contribute on your own project")
-
+      throw new BadRequestException(
+        'You cannot contribute on your own project',
+      );
 
     // if user already contribute on the project
 
     if (await this.isUserContributed({ userId, projectId: project.id }))
-      throw new ConflictException(this.userAlreadyContributed)
+      throw new ConflictException(this.userAlreadyContributed);
 
     try {
       // if not start process
@@ -65,61 +77,109 @@ export class ContributeService {
         data: {
           userId: userId,
           projectId: project.id,
-        }
-      })
-
+        },
+      });
     } catch (err) {
       if (err instanceof PrismaClientKnownRequestError)
-        throw new BadRequestException(err.meta.cause)
+        throw new BadRequestException(err.meta.cause);
 
       this.logger.error(err);
-      throw new InternalServerErrorException()
+      throw new InternalServerErrorException();
     }
-    return { success: true }
+    return { success: true };
   }
-
 
   async getProjectContributers(params: {
-    projectName: string,
-    username: string,
-    userId?: number
+    projectName: string;
+    username: string;
+    userId?: number;
   }) {
-    const { projectName, username, userId } = params;
-    const project = await this.prisma.project.findFirst({
-      where: {
+    const { userId, projectName, username } = params;
+
+    try {
+      const project = await this.projectService.getProjectByName({
+        isAccessToPrivate: true,
         name: projectName,
-        owner: { username }
-      }
-    })
+        username: username,
+      });
 
-    if (!project)
-      throw new NotFoundException(this.projectNotFoundErr)
+      if (!project) throw new NotFoundException(this.projectNotFoundErr);
 
-    if ((!userId && !project.isPublic) || (!project.isPublic && project.ownerId !== userId))
-      throw new UnauthorizedException("You cannot Access this Project")
+      if (!project.isPublic && project.ownerId !== userId)
+        throw new UnauthorizedException(this.CannotAccessThisProject);
 
+      const contributers = await this.prisma.projectContributer.findMany({
+        where: {
+          projectId: project.id,
+        },
 
-    const contributers = await this.prisma.projectContributer.findMany({
-      where: {
-        projectId: project.id
-      },
+        include: {
+          user: {
+            select: {
+              username: true,
+              profile: true,
+              id: true,
+              display_name: true,
+            },
+          },
+        },
+      });
 
-      include: {
-        project: true,
-        user: {
-          select: {
-            username: true,
-            role: true,
-            profile: true,
-            id: true,
-            display_name: true
-          }
-        }
-      }
-    })
-
-    return contributers;
+      return contributers;
+    } catch (err) {
+      if (err instanceof HttpException) throw err;
+      this.logger.error(err);
+      throw new InternalServerErrorException(err.message);
+    }
   }
 
+  private deleteContributer(
+    userId: number,
+    projectId: number,
+  ): Promise<ProjectContributer | null> {
+    return this.prisma.projectContributer.delete({
+      where: {
+        projectId_userId: {
+          projectId,
+          userId,
+        },
+      },
+    });
+  }
 
+  async kickContributer(params: {
+    owner: number;
+    projectName: string;
+    username: string;
+    userId: number;
+  }) {
+    const { userId, projectName, username, owner } = params;
+    try {
+      const project = await this.projectService.getProjectByName({
+        username: username,
+        isAccessToPrivate: true,
+        name: projectName,
+      });
+
+      if (project.ownerId !== owner)
+        throw new UnauthorizedException(this.CannotAccessThisProject);
+
+      const isContributerInThisProject =
+        await this.prisma.projectContributer.findFirst({
+          where: {
+            projectId: project.id,
+            userId: userId,
+          },
+        });
+
+      if (!isContributerInThisProject)
+        throw new BadRequestException(this.ContributerDoesNotFound);
+
+      const result = await this.deleteContributer(userId, project.id);
+      return result;
+    } catch (err) {
+      if (err instanceof HttpException) throw err;
+      this.logger.error(err);
+    }
+  }
 }
